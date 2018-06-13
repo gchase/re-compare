c7fd76bf785e91c99af08553fa1f8161274c50df
gchase <giladchase@gmail.com>
2018-06-13 11:13:17 +0300

    Change (temp) installation

---
+++ utils/install.sh
@@ -0,0 +1,17 @@
+# RE2-DEPENDINCIES
+pip3 install matplotlib numpy pandas ply tqdm pytest cython
+sudo apt install python3-tk
+
+# BASELINE ALGORITHM DEPENDINCIES
+# install re2
+git clone https://code.googlesource.com/re2
+cd re2
+make
+sudo make install
+cd ..
+rm -rf re2
+
+pip3 install https://github.com/andreasvc/pyre2/archive/master.zip
+
+
+
